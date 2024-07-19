990fe698441649a7727ef568e3a54960457c2523
unknown <nikkalyan52@gmail.com>
2024-07-20 02:24:30 +0530

    nav responsive

--- public/index.js
+++ public/index.js
@@ -1,1 +1,12 @@
 // index .js
+
+// nav bar  burger 
+
+const toggleButton = document.querySelector('.toggle-button');
+const navbarLinks = document.querySelector('.navbar-links');
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active');
+});
+
+// nav burger finish
